4b966f2f5100ca59480de0d4c60b89193347d2d8
It ToPlays <art2535452523@gmail.com>
2025-06-02 23:28:02 -0300

    Adcionei coisa pra kct família (organizei também)

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,13 +3,28 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from '../pages/Welcome';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-import Inventario from '../pages/Inventario';
+
+//Perfil ----
 import UserPerfil from '../pages/Perfil';
 import PerfilConfig from '../pages/Perfil/perfilConfig';
 import Suporte from '../pages/Perfil/suporte';
 import EscambosFeitos from '../pages/Perfil/escamboFeitos';
 import ComprovanteTroca from "../pages/Perfil/comprovante";
+
+// Inventario ----
+import Inventario from '../pages/Inventario';
 import addItem from "../pages/Inventario/addItem";
+import verItem from "../pages/Inventario/verItem";
+import editarItem from "../pages/Inventario/editarItem";
+
+// Chat ----
+import contatos from "../pages/Chat";
+import chat from "../pages/Chat/chat";
+
+// Pra se localizar melhor (daqui pra frente terá muita página) dá um ctrl+F e escreve uma das categorias abaixo:
+// - Perfil
+// - inventário 
+// - Chat
 
 
 const Stack = createNativeStackNavigator();
@@ -18,6 +33,8 @@ export default function Routes() {
   return (
     <Stack.Navigator initialRouteName="Inventario">
       
+      {/* ---------- Parte do Login ---------- */}
+
       <Stack.Screen
         name="Welcome"
         component={Welcome}
@@ -36,12 +53,7 @@ export default function Routes() {
         options={{ headerShown: false }}
       />
 
-      <Stack.Screen
-        name="Inventario"
-        component={Inventario}
-        options={{ headerShown: false }}
-      />
-
+      {/* ---------- Parte do Perfil ---------- */}
       <Stack.Screen
         name="UserPerfil"
         component={UserPerfil}
@@ -72,9 +84,42 @@ export default function Routes() {
         options={{ headerShown: false }}
       />
 
+      {/* ---------- Parte do inventário ---------- */}
+      <Stack.Screen
+        name="Inventario"
+        component={Inventario}
+        options={{ headerShown: false }}
+      />
+
       <Stack.Screen
         name="addItem"
         component={addItem}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="verItem"
+        component={verItem}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="editarItem"
+        component={editarItem}
+        options={{ headerShown: false }}
+      />
+
+      {/* ---------- Parte do Chat ---------- */}
+
+      <Stack.Screen
+        name="contatos"
+        component={contatos}
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="chat"
+        component={chat}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
---
+++ src/pages/inventario/editarItem.js
@@ -0,0 +1,2 @@
+//Apenas vazio por enquanto pois vai ser praticamente o mesmo do addItem.js
+// A diferença será de apenas algumas modificações e adições então será necessário ter a outra parte primeiro
